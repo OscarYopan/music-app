f63b1bc8476de4f2b2da5060386c892d90294617
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-08-28 17:20:58 -0300

    --

--- src/components/Animation/AnimationContainer.jsx
+++ src/components/Animation/AnimationContainer.jsx
@@ -3,7 +3,7 @@ import './AnimationContainer.css'
 export const AnimationContainer = () => {
   return (
     <div className='image-container'>
-      <img src="" alt="" />
+      <img src="https://images.unsplash.com/photo-1524678606370-a47ad25cb82a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80" alt="" />
     </div>
   )
 }
