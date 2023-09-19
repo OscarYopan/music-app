3743f2ff9598169088afdcd385483b2ce98c08e6
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-09-19 17:26:29 -0300

    --

--- src/components/Animation/AnimationContainer.jsx
+++ src/components/Animation/AnimationContainer.jsx
@@ -3,7 +3,7 @@ import './AnimationContainer.css'
 export const AnimationContainer = () => {
   return (
     <div className='image-container'>
-      <div className='sound-image'>{/* Image */}</div>
+      <div className='sound-image'>{/* Image on CSS BackGround*/}</div>
     </div>
   )
 }
