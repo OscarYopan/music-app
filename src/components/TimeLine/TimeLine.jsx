f4de1b14c2e457035370e68222f75eee251342e5
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-09-21 18:57:31 -0300

    --

--- src/components/TimeLine/TimeLine.jsx
+++ src/components/TimeLine/TimeLine.jsx
@@ -3,9 +3,13 @@ import './TimeLine.css'
 export const TimeLine = () => {
   return (
     <div className="timeline">
+      <p>00:00</p>
       <div className="line">
-        {/* content */}
+        <div className='timer'>
+          {/* content */}
+        </div>
       </div>
+      <p>03:48</p>
     </div>
   )
 }
