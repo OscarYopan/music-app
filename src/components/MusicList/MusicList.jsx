c2932f15739523e4e6878561f49595665369aa8b
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-09-24 17:54:35 -0300

    Update MusicList.jsx

--- src/components/MusicList/MusicList.jsx
+++ src/components/MusicList/MusicList.jsx
@@ -9,8 +9,8 @@ export const MusicList = () => {
             <img src="https://images.unsplash.com/photo-1606403444347-fdd6b74492d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="Album Cover" />
           </div>
           <div className='music-info'>
-            <h5 className='music-artist'>Engenheiros do Hawaii</h5>
             <p className='music-title'>Exercito de um Homem Só</p>
+            <h5 className='music-artist'>Engenheiros do Hawaii</h5>
           </div>
           <span className='music-time'>5:25</span>
         </li>
@@ -20,8 +20,8 @@ export const MusicList = () => {
             <img src="https://images.unsplash.com/photo-1606403444347-fdd6b74492d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="Album Cover" />
           </div>
           <div className='music-info'>
-            <h5 className='music-artist'>Nenhum de Nos</h5>
             <p className='music-title'>Da Janela</p>
+            <h5 className='music-artist'>Nenhum de Nos</h5>
           </div>
           <span className='music-time'>4:36</span>
         </li>
@@ -31,8 +31,8 @@ export const MusicList = () => {
             <img src="https://images.unsplash.com/photo-1606403444347-fdd6b74492d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="Album Cover" />
           </div>
           <div className='music-info'>
-            <h5 className='music-artist'>Queen</h5>
             <p className='music-title'>Love of my life</p>
+            <h5 className='music-artist'>Queen</h5>
           </div>
           <span className='music-time'>3:48</span>
         </li>
@@ -42,8 +42,8 @@ export const MusicList = () => {
             <img src="https://images.unsplash.com/photo-1606403444347-fdd6b74492d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="Album Cover" />
           </div>
           <div className='music-info'>
-            <h5 className='music-artist'>Ira</h5>
             <p className='music-title'>Envelheço na Cidade</p>
+            <h5 className='music-artist'>Ira</h5>
           </div>
           <span className='music-time'>5:25</span>
         </li>    
