d52e8b8a878378c7873c5e536d738fe708dcc74f
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-09-25 19:43:28 -0300

    --

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -5,18 +5,22 @@ export const Head = () => {
     <div className='head'>
       <div className='icon'>
         <i className="fa-solid fa-house"></i>
+        <p>Home</p>
       </div>
 
       <div className='icon'>
         <i className="fa-solid fa-book"></i>
+        <p>Library</p>
       </div>
 
       <div className='icon'>
         <i className="fa-solid fa-magnifying-glass"></i>
+        <p>Search</p>
       </div>
 
       <div className='icon'>
         <i className="fa-solid fa-gear"></i>
+        <p>Settings</p>
       </div>
     </div>
   )
