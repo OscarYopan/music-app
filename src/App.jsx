359adfab6e5cb7936fa8470738f950ffecaec310
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-09-26 15:24:45 -0300

    --+

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <div className='player-container'>
         <Head />
-        {/* <Home /> */}
+        <Home />
       </div>
     </>
   )
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,15 @@ import './Home.css'
 export const Home = () => {
   return (
     <div className='home-container'>
-      Teste
+      <div className='player-img'>
+        <img src="" alt="" />
+        <h3>Music Name</h3>
+        <h4>Artist Name</h4>
+      </div>
+      <button>
+        <i className="fa-solid fa-play"></i>
+        <i className="fa-solid fa-pause"></i>
+      </button>
     </div>
   )
 }
