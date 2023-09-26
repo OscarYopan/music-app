27a6294c2bfa0ace648e8d108d384e2955d9f74c
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-09-26 16:04:12 -0300

    --

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ export const Home = () => {
   return (
     <div className='home-container'>
       <div className='music-img'>
-        <img src="https://images.unsplash.com/photo-1617136778830-e15aea8ba3f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2105&q=80" alt="Engenheiros do Hawwai ALbum Pic" />
+        <img src="https://images.unsplash.com/photo-1581742009630-afd6768aa98e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=600&q=60" alt="Engenheiros do Hawwai ALbum Pic" />
       </div>
       <div className='music-info'>
         <h3>Music Name</h3>
