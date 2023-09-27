e4d3f4872e74e04718036451478516f19f20dbab
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-09-27 15:28:32 -0300

    Update Head.jsx

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -1,7 +1,4 @@
 import './Head.css'
-import { Controls } from '../Controls/Controls'
-import { Home } from '../Home/Home'
-import { MusicList } from '../MusicList/MusicList'
 
 export const Head = () => {
   function openHome() {
@@ -12,17 +9,14 @@ export const Head = () => {
     <div className='head'>
       <div className='icon' onClick={openHome}>
         <i className="fa-solid fa-house"></i>
-        <Home />
       </div>
 
       <div className='icon'>
         <i className="fa-solid fa-book"></i>
-        <MusicList />
       </div>
 
       <div className='icon'>
         <i className="fa-solid fa-magnifying-glass"></i>
-        <Controls />
       </div>
     </div>
   )
