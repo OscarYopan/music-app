dfc074794ce936f0abaf329c170196fad6941639
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-09-27 15:23:19 -0300

    Update Head.jsx

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -1,23 +1,29 @@
 import './Head.css'
+import { Controls } from '../Controls/Controls'
+import { Home } from '../Home/Home'
+import { MusicList } from '../MusicList/MusicList'
 
 export const Head = () => {
+  function openHome() {
+    // <Home />
+  }
+
   return (
     <div className='head'>
-      <div className='icon'>
+      <div className='icon' onClick={openHome}>
         <i className="fa-solid fa-house"></i>
+        <Home />
       </div>
 
       <div className='icon'>
         <i className="fa-solid fa-book"></i>
+        <MusicList />
       </div>
 
       <div className='icon'>
         <i className="fa-solid fa-magnifying-glass"></i>
+        <Controls />
       </div>
-
-      {/* <div className='icon'>
-        <i className="fa-solid fa-gear"></i>
-      </div> */}
     </div>
   )
 }
