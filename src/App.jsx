522b6f4fef3558ee6daac1bc596dc353b22eb428
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-09-28 19:44:22 -0300

    --

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
 import './App.css'
-import { Head } from './components/Head/Head'
-// import { Home } from './components/Home/Home'
-
+import './components/Head/Head.css'
+import { useState } from 'react';
+import { Home } from './components/Home/Home'
 
 function App() {
+  const [homeShow, setHomeShow] = useState(false);
+  // const [favShow, setFavtShow] = useState(false);
+
+  const handleClickHome = () => {
+    setHomeShow(current => !current);
+  };
+
+  // const handleClickFav = () => {
+  //   setFavtShow(current => !current);
+  // };
   return (
     <>
       <div className='player-container'>
-        <Head />
+        <div className='head'>
+          <div
+            className='icon'
+            onClick={handleClickHome}
+          >
+            <i className="fa-solid fa-house"></i>
+          </div>
+          <div
+            className='icon'
+            // onMouseEnter={handleClickCart}
+            // onMouseLeave={handleClickCart}
+          >
+            <i className="fa-solid fa-book"></i>
+            {/* {cartShow && <Cart />} */}
+          </div>
+
+          <div className='icon'>
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </div>
+        </div>
+
+        {homeShow && <Home />}
+
+        
       </div>
     </>
   )
