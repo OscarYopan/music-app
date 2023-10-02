99d97869f53b50c26c2c878d9a70ca9e1b928a05
Oscar Luis Yopan <oscar.yopan@outlook.com>
2023-10-02 14:53:44 -0300

    Update Head.jsx

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -1,38 +1,18 @@
-import { useState } from 'react';
-import { Home } from '../Home/Home'
 import './Head.css'
 
 export const Head = () => {
-  const [homeShow, setHomeShow] = useState(false);
-  // const [favShow, setFavtShow] = useState(false);
-
-  const handleClickHome = () => {
-    setHomeShow(current => !current);
-  };
-
-  // const handleClickFav = () => {
-  //   setFavtShow(current => !current);
-  // };
-
   return (
     <div className='head'>
       <div
         className='icon'
-        onClick={handleClickHome}
-        // onMouseEnter={handleClickHome}
-        // onMouseLeave={handleClickHome}
       >
         <i className="fa-solid fa-house"></i>
-        {homeShow && <Home />}
       </div>
 
       <div
         className='icon'
-        // onMouseEnter={handleClickCart}
-        // onMouseLeave={handleClickCart}
       >
         <i className="fa-solid fa-book"></i>
-        {/* {cartShow && <Cart />} */}
       </div>
 
       <div className='icon'>
